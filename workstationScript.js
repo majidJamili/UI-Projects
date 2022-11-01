cbfa35fef3dd4100bf54866c03469a8fc78f0127
majidjamili <jamilimajid@gmail.com>
2022-11-01 13:20:32 +1000

    Added Workstation Style - Ongoing

---
+++ workstationScript.js
@@ -0,0 +1,32 @@
+
+
+function dragStart(e){
+    this.style.opacity = '0.4';
+}
+
+function dragEnd(e){
+    this.style.opacity ='1'; 
+}
+
+let tasks = document.querySelectorAll('.container .draggable'); 
+console.log(tasks); 
+
+tasks.forEach( task => {
+    task.addEventListener('dragstart', dragStart);
+    task.addEventListener('dragend', dragEnd);    
+});
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
